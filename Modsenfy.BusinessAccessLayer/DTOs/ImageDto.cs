46de84637a3d82f3062de727ae66125bdc6484b9
VlazhV <danilabondarev1234@gmail.com>
2023-06-17 14:23:36 +0300

    renamed prop

--- Modsenfy.BusinessAccessLayer/DTOs/ImageDto.cs
+++ Modsenfy.BusinessAccessLayer/DTOs/ImageDto.cs
@@ -2,6 +2,6 @@ namespace Modsenfy.BusinessAccessLayer.DTOs;
 
 public class ImageDto
 {
-	public string Type { get; set; }
+	public string ImageType { get; set; }
 	public string Filename { get; set; }
 }
