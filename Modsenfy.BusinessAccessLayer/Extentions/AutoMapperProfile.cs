bf9405f5be77435f6e15f5eae47f74a15c2f3596
VlazhV <danilabondarev1234@gmail.com>
2023-06-18 16:29:28 +0300

    added maps

--- Modsenfy.BusinessAccessLayer/Extentions/AutoMapperProfile.cs
+++ Modsenfy.BusinessAccessLayer/Extentions/AutoMapperProfile.cs
@@ -7,23 +7,59 @@ namespace Modsenfy.BusinessAccessLayer.Extentions;
 
 public class AutoMapperProfile : Profile
 {
-    public AutoMapperProfile()
-    {
-        CreateMap<Image, ImageDto>()
-            .ForMember(dest => dest.ImageType, opt => opt.MapFrom(src => src.ImageType));
-        
-        CreateMap<Audio, AudioDto>();
-        CreateMap<Album, AlbumDto>()
-            .ForMember(dest => dest.Image, opt => opt.MapFrom(src => src.Image));
+	public AutoMapperProfile()
+	{
+		
+		
+		CreateMap<Image, ImageDto>()
+			.ForMember(dest => dest.ImageType, opt => opt.MapFrom(src => src.ImageType.ImageTypeName));
+
+
+		CreateMap<Audio, AudioDto>()
+			.ForMember(dest => dest.AudioFilename, opt => opt.MapFrom(src => src.AudioFilename));
+
+        CreateMap<Genre, GenreDto>()
+            .ForMember(dest => dest.Name, opt => opt.MapFrom(src => src.GenreName));
+
+
         CreateMap<Artist, ArtistDto>()
-            .ForMember(dest => dest.Image, opt => opt.MapFrom(src => src.Image));
+			.ForMember(dest => dest.Image, opt => opt.MapFrom(src => src.Image));
+			
+		
+		CreateMap<Track, TrackDto>()
+			.ForMember(dest => dest.Artists, opt => opt.MapFrom(src => src.TrackArtists.Select(ta => ta.Artist)))
+			.ForMember(dest => dest.Genre, opt => opt.MapFrom(src => src.Genre.GenreName))
+			.ForMember(dest => dest.Audio, opt => opt.MapFrom(src => src.Audio));
+			
 
-        CreateMap<Track, TrackWithAlbumDto>()
-            .ForMember(dest => dest.Artists, opt => opt.MapFrom(src => src.TrackArtists.Select(ta => ta.Artist)))
-            .ForMember(dest => dest.Album, opt => opt.MapFrom(src => src.Album))
-            .ForMember(dest => dest.Audio, opt => opt.MapFrom(src => src.Audio));
+		CreateMap<Album, AlbumWithTracksDto>()
+			.ForMember(dest => dest.Image, opt => opt.MapFrom(src => src.Image))
+			.ForMember(dest => dest.AlbumRelease, opt => opt.MapFrom(src => DateOnly.FromDateTime(src.AlbumRelease)))
+			.ForMember(dest => dest.AlbumType, opt => opt.MapFrom(src => src.AlbumType.AlbumTypeName))
+			.ForMember(dest => dest.Artist, opt => opt.MapFrom(src => src.Artist))
+			.ForMember(dest => dest.Tracks, opt => opt.MapFrom(src => src.Tracks));
+			
+		
 
-        CreateMap<TrackDto, Track>();
+		CreateMap<Album, AlbumDto>()
+			.ForMember(dest => dest.Image, opt => opt.MapFrom(src => src.Image))
+			.ForMember(dest => dest.AlbumRelease, opt => opt.MapFrom(src =>
+			DateOnly.FromDateTime(src.AlbumRelease)))
+			.ForMember(dest => dest.AlbumType, opt => opt.MapFrom(src => src.AlbumType.AlbumTypeName))
+			.ForMember(dest => dest.Artist, opt => opt.MapFrom(src => src.Artist));
 
-    }
+		CreateMap<Track, TrackWithAlbumDto>()
+			.ForMember(dest => dest.Artists, opt => opt.MapFrom(src => src.TrackArtists.Select(ta => ta.Artist)))
+			.ForMember(dest => dest.Album, opt => opt.MapFrom(src => src.Album))
+			.ForMember(dest => dest.Audio, opt => opt.MapFrom(src => src.Audio));
+
+		CreateMap<TrackDto, Track>();
+		
+		var configuration = new MapperConfiguration(cfg =>
+		{
+			cfg.RecognizePrefixes("Album");
+		});
+		configuration.CompileMappings();
+
+	}
 }
