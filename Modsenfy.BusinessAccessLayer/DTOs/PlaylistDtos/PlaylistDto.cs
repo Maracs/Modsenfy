de1d77e2d8348242c96de651227afb668c8105be
VlazhV <danilabondarev1234@gmail.com>
2023-06-20 21:02:20 +0300

    added Duration

--- Modsenfy.BusinessAccessLayer/DTOs/PlaylistDtos/PlaylistDto.cs
+++ Modsenfy.BusinessAccessLayer/DTOs/PlaylistDtos/PlaylistDto.cs
@@ -6,7 +6,7 @@ public class PlaylistDto
 	public string Name { get; set; }
 	public string Release { get; set; }
 	public UserDto Owner { get; set; }
-	public IEnumerable<PlaylistFollowersDto> Followers { get; set; }
+	public PlaylistFollowersDto Followers { get; set; }
     public IEnumerable<TrackDto> Tracks { get; set; }
 	
 
