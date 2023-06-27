2a6f932c57687885fba056a87a6ea04a9cb71e69
Veteril <stepan.shandrakov@gmail.com>
2023-06-27 04:17:22 +0300

    authorization-added

---
+++ Modsenfy.BusinessAccessLayer/DTOs/UserDtos/UserSigningDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Modsenfy.BusinessAccessLayer.DTOs.UserDtos;
+public class UserSigningDto
+{
+    public string UserNickname { get; set; }
+
+    public string Password { get; set; }
+}
+
