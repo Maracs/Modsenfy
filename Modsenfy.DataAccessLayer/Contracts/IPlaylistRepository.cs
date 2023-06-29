9fd81fa1fcaf57bb9deeb5b53c1d02fc697efcb5
Chykiliou Dzmitry <gvcvolley.ru@gmail.com>
2023-06-29 21:25:39 +0300

    +playlist +artist_fixes

--- Modsenfy.DataAccessLayer/Contracts/IPlaylistRepository.cs
+++ Modsenfy.DataAccessLayer/Contracts/IPlaylistRepository.cs
@@ -2,8 +2,14 @@ using Modsenfy.DataAccessLayer.Entities;
 
 namespace Modsenfy.DataAccessLayer.Contracts
 {
-    internal interface IPlaylistRepository : IRepository<Playlist>
+    public interface IPlaylistRepository : IRepository<Playlist>
     {
-
+        Task<Playlist> GetById(int id);
+        Task<Playlist> GetByIdWithJoins(int id);
+        Task<IEnumerable<Playlist>> GetAll();
+        Task SaveChanges();
+        Task Create(Playlist entity);
+        Task Update(Playlist entity);
+        void Delete(Playlist entity);
     }
 }
--- Modsenfy.PresentationLayer/Controllers/PlaylistController.cs
+++ Modsenfy.PresentationLayer/Controllers/PlaylistController.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Mvc;
 using Modsenfy.BusinessAccessLayer.DTOs;
 using Modsenfy.DataAccessLayer.Entities;
 using Modsenfy.DataAccessLayer.Repositories;
+using Modsenfy.BusinessAccessLayer.Services;
 
 namespace Modsenfy.PresentationLayer.Controllers
 {
@@ -11,12 +12,103 @@ namespace Modsenfy.PresentationLayer.Controllers
     public class PlaylistController : ControllerBase
     {
         private readonly PlaylistRepository _playlistRepository;
+        private readonly PlaylistService _playlistService;
         private readonly IMapper _mapper;
 
-        public PlaylistController(IMapper mapper, PlaylistRepository playlistRepository)
+        public PlaylistController(PlaylistRepository playlistRepository, PlaylistService playlistService, IMapper mapper)
         {
-            _mapper = mapper;
             _playlistRepository = playlistRepository;
+            _playlistService = playlistService;
+            _mapper = mapper;
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<PlaylistDto>> GetArtist(int id)
+        {
+            var playlist = await _playlistRepository.GetByIdWithJoins(id);
+
+            if (playlist is null)
+            {
+                return BadRequest();
+            }
+
+            var playlistDto = _mapper.Map<ArtistDto>(playlist);
+
+            return Ok(playlistDto);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> CreatePlaylist(PlaylistDto playlistDto)
+        {
+            var playlist = _mapper.Map<Playlist>(playlistDto);
+
+            if (playlist is null)
+            {
+                return BadRequest();
+            }
+
+            await _playlistRepository.Create(playlist);
+            await _playlistRepository.SaveChanges();
+
+            return Ok();
+        }
+
+        [HttpPut("{id}")]
+        public async Task<ActionResult> UpdatePlaylist(PlaylistDto playlistDto)
+        {
+            var playlist = _mapper.Map<Playlist>(playlistDto);
+
+            if (playlist is null)
+            {
+                return BadRequest();
+            }
+            
+            await _playlistRepository.Update(playlist);
+            await _playlistRepository.SaveChanges();
+
+            return Ok();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult> DeletePlaylist(int id)
+        {
+            var playlist = await _playlistRepository.GetById(id);
+
+            if (playlist is null)
+            {
+                return BadRequest();
+            }
+            
+            _playlistRepository.Delete(playlist);
+            await _playlistRepository.SaveChanges();
+
+            return Ok();
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<PlaylistDto>>> GetSeveralPlaylists(List<int> ids)
+        {
+            var playlistDtos = await _playlistService.GetSeveralPlaylists(ids);
+
+            if (playlistDtos is null)
+            {
+                return BadRequest();
+            }
+
+            return Ok(playlistDtos);
+        }
+
+        [HttpGet("{id}/tracks")]
+        public async Task<ActionResult<IEnumerable<TrackDto>>> GetTracksOfPlaylist(int id)
+        {
+            var trackDtos = await _playlistService.GetTracksOfPlaylist(id);
+
+            if (trackDtos is null)
+            {
+                return BadRequest();
+            }
+
+            return Ok(trackDtos);
         }
     }
 }
