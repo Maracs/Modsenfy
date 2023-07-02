d92a16ab8bb85db41a1b1b75f5fd534955b39521
VlazhV <danilabondarev1234@gmail.com>
2023-07-02 17:51:16 +0300

    fix search

--- Modsenfy.BusinessAccessLayer/Services/SearchService.cs
+++ Modsenfy.BusinessAccessLayer/Services/SearchService.cs
@@ -8,41 +8,41 @@ namespace Modsenfy.BusinessAccessLayer.Services;
 
 public class SearchService
 {
-    private readonly AlbumRepository _albumRepository;
-    private readonly ArtistRepository _artistRepository;
-    private readonly TrackRepository _trackRepository;
-    private readonly IMapper _mapper;
-    public SearchService(
-        AlbumRepository albumRepository,
-        ArtistRepository artistRepository,
-        TrackRepository trackRepository,
-        IMapper mapper
-        )
-    {
-        _albumRepository = albumRepository;
-        _artistRepository = artistRepository;
-        _trackRepository = trackRepository;
-        _mapper = mapper;
-    }
+	private readonly AlbumRepository _albumRepository;
+	private readonly ArtistRepository _artistRepository;
+	private readonly TrackRepository _trackRepository;
+	private readonly IMapper _mapper;
+	public SearchService(
+		AlbumRepository albumRepository,
+		ArtistRepository artistRepository,
+		TrackRepository trackRepository,
+		IMapper mapper
+		)
+	{
+		_albumRepository = albumRepository;
+		_artistRepository = artistRepository;
+		_trackRepository = trackRepository;
+		_mapper = mapper;
+	}
 
-    public async Task<SearchDto> Search(string query)
-    {
-        var albums = await _albumRepository.GetAll();
-    
-        IEnumerable<Searchable> albumSearchables = new List<Searchable>();
-        foreach (var album in albums)
-        {
-            albumSearchables.Append(new Searchable(album, album.GetType().GetProperty(nameof(Album.AlbumName)), query));
-        }
-        albumSearchables = albumSearchables.OrderByDescending(a => a.Rate);
+	public async Task<SearchDto> Search(string query)
+	{
+		var albums = await _albumRepository.GetAll();
+	
+		IEnumerable<Searchable> albumSearchables = new List<Searchable>();
+		foreach (var album in albums)
+		{
+			albumSearchables = albumSearchables.Append(new Searchable(album, album.GetType().GetProperty(nameof(Album.AlbumName)), query));
+		}
+		albumSearchables = albumSearchables.OrderByDescending(a => a.Rate);
 
-        var tracks = await _trackRepository.GetAll();
+		var tracks = await _trackRepository.GetAll();
 
 		IEnumerable<Searchable> trackSearchables = new List<Searchable>();
 		
 		foreach (var track in tracks)
 		{
-			trackSearchables.Append(new Searchable(track, track.GetType().GetProperty(nameof(Track.TrackName)), query));
+			trackSearchables = trackSearchables.Append(new Searchable(track, track.GetType().GetProperty(nameof(Track.TrackName)), query));
 		}
 		trackSearchables = trackSearchables.OrderByDescending(t => t.Rate);
 
@@ -52,10 +52,11 @@ public class SearchService
 		IEnumerable<Searchable> artistSearchables = new List<Searchable>();
 		foreach (var artist in artists)
 		{
-			artistSearchables.Append(new Searchable(artist, artist.GetType().GetProperty(nameof(Artist.ArtistName)), query));
+			artistSearchables = artistSearchables.Append(new Searchable(artist, artist.GetType().GetProperty(nameof(Artist.ArtistName)), query));
 		}
+        artistSearchables = artistSearchables.OrderByDescending(a => a.Rate);
 
-		var searchDto = new SearchDto()
+        var searchDto = new SearchDto()
 		{
 			Albums = albumSearchables.Select(a => _mapper.Map<AlbumDto>((Album)a.SearchObject)),
 			Tracks = trackSearchables.Select(t => _mapper.Map<TrackDto>((Track)t.SearchObject)),
@@ -64,6 +65,6 @@ public class SearchService
 		
 		
 
-        return searchDto;
-    }
+		return searchDto;
+	}
 }
