9313380f876f8e8a8bbb4420c45b10459dd65b95
VlazhV <danilabondarev1234@gmail.com>
2023-07-02 17:51:16 +0300

    fix search

--- Modsenfy.BusinessAccessLayer/Search/Searchable.cs
+++ Modsenfy.BusinessAccessLayer/Search/Searchable.cs
@@ -8,50 +8,52 @@ namespace Modsenfy.BusinessAccessLayer.Search;
 
 public class Searchable
 {
-    private float _rate = -1f;
-    public object SearchObject { get; }
-    public PropertyInfo SearchProperty { get; }
-    public string Query { get; }
-    public float Rate
-    {
-        get
-        {
-            if (_rate == -1f)
-                _rate = CountRate();
-            return _rate;
-        }
-    }
+	private float _rate;
+	public object SearchObject { get; }
+	public PropertyInfo SearchProperty { get; }
+	public string Query { get; }
+	public float Rate
+	{
+		get
+		{
+			return _rate;
+		}
+	}
 
-    public Searchable(object searchObject, PropertyInfo searchProperty, string query)
-    {
-        SearchObject = searchObject;
-        SearchProperty = searchProperty;
-        Query = query;
-    }
+	public Searchable(object searchObject, PropertyInfo searchProperty, string query)
+	{
+		SearchObject = searchObject;
+		SearchProperty = searchProperty;
+		Query = query;
+		_rate = CountRate();
+	}
 
-    private float CountRate()
-    {
-        float rate = 0f;
+	private float CountRate()
+	{
+		float rate = 0f;
 
-        var queryWords = Query.ToLowerInvariant().Split(new char[] { ' ' });
-        var nameWords = ((string)SearchProperty.GetValue(SearchObject)).ToLowerInvariant().Split(new char[] { ' ' });
+		var queryWords = Query.ToLowerInvariant().Split(new char[] { ' ' });
+		var name = ((string)SearchProperty.GetValue(SearchObject));
+		var nameWords = name.ToLowerInvariant().Split(new char[] { ' ' });
 
-        List<float> rateTerms = new List<float>();
-        foreach (var nameWord in nameWords)
-        {
-            foreach (var queryWord in queryWords)
-            {
-                while (!nameWord.Contains(queryWord) && queryWord.Length > 0)
-                {
-                    queryWord.Remove(queryWord.Length - 1);
-                }
-                rateTerms.Add(queryWord.Length / nameWord.Length);
-            }
-        }
+		List<int> lengthsOfHits = new List<int>();
+		foreach (var nameWord in nameWords)
+		{
+			foreach (var queryWord in queryWords)
+			{
+				var qw = queryWord;
+				while (!nameWord.Contains(qw) && qw.Length > 0)
+				{
+					qw = qw.Remove(qw.Length - 1);
+				}
+				lengthsOfHits.Add(qw.Length);
+			}
+		}
 
-        foreach (var rateTerm in rateTerms)
-            rate += rateTerm;
-        rate /= rateTerms.Count;
+		
+		foreach (var l in lengthsOfHits)
+			rate += l;
+        rate /= (name.Length - (nameWords.Count() - 1));
         return rate;
-    }
+	}
 }
