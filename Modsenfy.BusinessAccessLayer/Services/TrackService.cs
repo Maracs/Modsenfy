207e3cb166aa94f13f5483039c3e5fb099504e8c
Veteril <stepan.shandrakov@gmail.com>
2023-07-03 22:52:52 +0300

    track-controller-updated-+-some-refactoring

--- Modsenfy.BusinessAccessLayer/Services/TrackService.cs
+++ Modsenfy.BusinessAccessLayer/Services/TrackService.cs
@@ -1,5 +1,6 @@
 using AutoMapper;
 using Modsenfy.BusinessAccessLayer.DTOs;
+using Modsenfy.DataAccessLayer.Contracts;
 using Modsenfy.DataAccessLayer.Entities;
 using Modsenfy.DataAccessLayer.Repositories;
 
@@ -7,66 +8,98 @@ namespace Modsenfy.BusinessAccessLayer.Services;
 
 public class TrackService
 {
-	private readonly TrackRepository _trackRepository;
-	private readonly GenreRepository _genreRepository;
-	private readonly ArtistRepository _artistRepository;
-	private readonly AudioRepository _audioRepository;
-	private readonly TrackArtistsRepository _trackArtistsRepository;
-	private readonly IMapper _mapper;
+    private readonly TrackRepository _trackRepository;
+    private readonly GenreRepository _genreRepository;
+    private readonly ArtistRepository _artistRepository;
+    private readonly AudioRepository _audioRepository;
+    private readonly TrackArtistsRepository _trackArtistsRepository;
+    private readonly IMapper _mapper;
 
-	public TrackService(
-		TrackRepository trackRepository, 
-		GenreRepository genreRepository,
-		ArtistRepository artistRepository, 
-		TrackArtistsRepository trackArtistsRepository,
-		AudioRepository audioRepository,
-		IMapper mapper)
-	{
-		_trackRepository = trackRepository;
-		_genreRepository = genreRepository;
-		_artistRepository = artistRepository;
-		_trackArtistsRepository = trackArtistsRepository;
+    public TrackService(
+        TrackRepository trackRepository,
+        GenreRepository genreRepository,
+        ArtistRepository artistRepository,
+        TrackArtistsRepository trackArtistsRepository,
+        AudioRepository audioRepository,
+        IMapper mapper)
+    {
+        _trackRepository = trackRepository;
+        _genreRepository = genreRepository;
+        _artistRepository = artistRepository;
+        _trackArtistsRepository = trackArtistsRepository;
         _audioRepository = audioRepository;
         _mapper = mapper;
-	}
+    }
 
-	public async Task CreateTrack(TrackCreateDto trackDto, int albumId, int artistOwnerId)
-	{
-		var genre = await _genreRepository.GetByName(trackDto.GenreName);
-		var audio = new Audio()
-		{
-			AudioFilename = trackDto.Audio.AudioFilename
-		};
+    public async Task CreateTrack(TrackCreateDto trackDto, int albumId, int artistOwnerId)
+    {
+        var genre = await _genreRepository.GetByName(trackDto.GenreName);
+        var audio = new Audio()
+        {
+            AudioFilename = trackDto.Audio.AudioFilename
+        };
 
         var addedAudio = await _audioRepository.CreateAndGetAsync(audio);
 
         var track = new Track()
-		{
-			TrackName = trackDto.TrackName,
-			TrackStreams = 0,
-			TrackDuration = DateTime.Parse("0:" + trackDto.TrackDuration),
-			TrackGenius = trackDto.TrackGenius,
-			AudioId = addedAudio.AudioId,
-			GenreId = genre.GenreId,
-			AlbumId = albumId
-		};
+        {
+            TrackName = trackDto.TrackName,
+            TrackStreams = 0,
+            TrackDuration = DateTime.Parse("0:" + trackDto.TrackDuration),
+            TrackGenius = trackDto.TrackGenius,
+            AudioId = addedAudio.AudioId,
+            GenreId = genre.GenreId,
+            AlbumId = albumId
+        };
 
+        var addedTrack = await _trackRepository.CreateAndGetAsync(track);
+        trackDto.Artists.Append(artistOwnerId);
+        foreach (var artistId in trackDto.Artists)
+        {
+            var artist = await _artistRepository.GetByIdAsync(artistId);
+            if (artist == null)
+                throw new Exception("artist not found");
 
-		var addedTrack = await _trackRepository.CreateAndGet(track);
-		trackDto.Artists.Append(artistOwnerId);
-		foreach (var artistId in trackDto.Artists)
-		{
-			var artist = await _artistRepository.GetByIdAsync(artistId);
-			if (artist == null)
-				throw new Exception("artist not found");
+            var trackArtists = new TrackArtists
+            {
+                TrackId = addedTrack.TrackId,
+                ArtistId = artistId
+            };
 
-			var trackArtists = new TrackArtists
-			{
-				TrackId = addedTrack.TrackId,
-				ArtistId = artistId
-			};
+            await _trackArtistsRepository.CreateAsync(trackArtists);
+        }
+    }
+    public async Task<IEnumerable<TrackWithAlbumDto>> GetSeverlTracksAsync(int limit, int offset, string ids)
+    {
+        IEnumerable<Track> tracks;
+        IEnumerable<int> intIds;
+        if (ids.Equals("all"))
+        {
+            if (limit == -1 && offset == 0)
+                tracks = await _trackRepository.GetAllAsync();
+            else if (limit == -1)
+                tracks = await _trackRepository.GetSkippedAsync(offset);
+            else
+                tracks = await _trackRepository.GetLimitedAsync(limit, offset);
+        }
+        else
+        {
+            var splittedIds = ids.Split(',');
+            intIds = splittedIds.Select(id => int.Parse(id));
+            tracks = new List<Track>();
+            foreach (var id in intIds)
+            {
+                var track = await _trackRepository.GetByIdWithJoinsAsync(id);
+                tracks = tracks.Append(track);
+            }
+        }
 
-      await _trackArtistsRepository.CreateAsync(trackArtists);
-		}
-	}
+        IEnumerable<TrackWithAlbumDto> trackDtos = new List<TrackWithAlbumDto>();
+        foreach (var track in tracks)
+        {
+            var trackDto = _mapper.Map<TrackWithAlbumDto>(track);
+            trackDtos = trackDtos.Append(trackDto);
+        }
+        return trackDtos;
+    }
 }
--- Modsenfy.PresentationLayer/Controllers/TracksController.cs
+++ Modsenfy.PresentationLayer/Controllers/TracksController.cs
@@ -1,8 +1,11 @@
 ﻿using AutoMapper;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore.Storage;
 using Modsenfy.BusinessAccessLayer.DTOs;
+using Modsenfy.BusinessAccessLayer.Extentions;
+using Modsenfy.BusinessAccessLayer.Services;
 using Modsenfy.DataAccessLayer.Data;
 using Modsenfy.DataAccessLayer.Entities;
 using Modsenfy.DataAccessLayer.Repositories;
@@ -16,17 +19,20 @@ namespace Modsenfy.PresentationLayer.Controllers
     {
         private readonly TrackRepository _trackRepository;
         private readonly IMapper _mapper;
+        private readonly TrackService _trackService;
 
-        public TracksController(IMapper mapper, TrackRepository trackRepository)
+        public TracksController(IMapper mapper, TrackRepository trackRepository, TrackService trackService)
         {
             _mapper = mapper;
             _trackRepository = trackRepository;
+            _trackService = trackService;
         }
 
+        [AllowAnonymous]
         [HttpGet("{id}")]
-        public async Task<ActionResult<TrackWithAlbumDto>> GetTrack(int id)
+        public async Task<ActionResult<TrackWithAlbumDto>> GetTrack([FromRoute]int id)
         {
-            var track = await _trackRepository.GetByIdWithJoins(id);
+            var track = await _trackRepository.GetByIdWithJoinsAsync(id);
             if (track == null)
                 return NotFound();
 
@@ -35,55 +41,49 @@ namespace Modsenfy.PresentationLayer.Controllers
             return Ok(trackDto);
         }
 
-        [HttpPost]
-        public async Task<ActionResult> CreateTrack(TrackDto trackDto)
+        [Authorize(Roles = "Artist")]
+        [HttpPut]
+        public async Task<ActionResult> UpdateTrack([FromBody]TrackDto trackDto)
         {
-            var track = new Track() 
-            {
-             
-            };
+            if (!(await _trackRepository.IsTrackOwnerAsync(User.GetUserId(), trackDto.TrackId)))
+                return Forbid();
 
-            await _trackRepository.CreateAsync(track);
-            await _trackRepository.SaveChangesAsync();
-
-            return Ok(track.TrackId);
-        }
-
-        [HttpPut("{id}")]
-        public async Task<ActionResult> UpdateTrack(TrackDto trackDto)
-        {
             var track = _mapper.Map<Track>(trackDto);
             await _trackRepository.UpdateAsync(track);
-            await _trackRepository.SaveChangesAsync();
 
             return Ok(track.TrackId);
         }
 
+        [Authorize(Roles = "Artist")]
         [HttpDelete("{id}")]
-        public async Task<ActionResult> DeleteTrack(int id)
+        public async Task<ActionResult> DeleteTrack([FromQuery]int id)
         {
+            if (!(await _trackRepository.IsTrackOwnerAsync(User.GetUserId(), id)))
+                return Forbid();
+
             var track = await _trackRepository.GetByIdAsync(id);
             _trackRepository.Delete(track);
             await _trackRepository.SaveChangesAsync();
             return Ok(track.TrackId);
         }
 
+        [AllowAnonymous]
         [HttpGet]
-        public async Task<ActionResult<IEnumerable<TrackWithAlbumDto>>> GetSeveralTracks(List<int> ids)
+        public async Task<ActionResult<IEnumerable<TrackWithAlbumDto>>> GetSeveralTracks([FromQuery] int limit = -1,
+            [FromQuery] int offset = 0, [FromQuery] string ids = "all")
         {
-            var tracks = await _trackRepository.GetSeverlTracks(ids);
-            List<TrackWithAlbumDto> tracksDto = new List<TrackWithAlbumDto>();
-            foreach (var track in tracks)
-            {
-                tracksDto.Add(_mapper.Map<TrackWithAlbumDto>(track));   
-            }
-            return Ok(tracksDto);
+            var tracks = await _trackService.GetSeverlTracksAsync(limit, offset, ids);
+            return Ok(tracks);
         }
-
+        
+        [Authorize(Roles = "Artist")]
         [HttpGet("{id}/streams")]
         public async Task<ActionResult<TrackWithStreamsDto>> GetTrackStreams(int id)
         {
-            var track = await _trackRepository.GetByIdWithStreams(id);
+            if (!(await _trackRepository.IsTrackOwnerAsync(User.GetUserId(), id)))
+                return Forbid();
+
+            var track = await _trackRepository.GetByIdWithStreamsAsync(id);
             var trackDto = _mapper.Map<TrackWithStreamsDto>(track);
             return Ok(trackDto);
         }
